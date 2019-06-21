967a05c660b6b2d355a8c5a89f9f7b6f5f0a2df3
William Weisberger <bill.weisberger@gmail.com>
2019-06-21 16:03:08 -0600

    Sixth commit - Part II, B complete

--- js/app.js
+++ js/app.js
@@ -114,7 +114,11 @@ for (let i = 11; i <= 4000; i += 3) {
 //
 // B. Get even
 // Print out the numbers that are within the range of 1 - 100.
-//
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i, " <--is an even number");
+  } else { console.log(i); }
+}
 // Adjust your code to add a message next to even numbers only that says: "<-- is an even number".
 //
 // 🔴 Commit.
