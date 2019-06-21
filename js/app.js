b01290e0f623ce4cfed53462516928e98702f784
William Weisberger <bill.weisberger@gmail.com>
2019-06-21 16:00:12 -0600

    Fifth commit - Part II, A complete

--- js/app.js
+++ js/app.js
@@ -99,11 +99,17 @@ if (age >= 16){
 //
 // A. The basics
 // Write a loop that will print out all the numbers from 0 to 10, inclusive.
-//
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
 // Write a loop that will print out all the numbers from 10 up to and including 400.
-//
+for (let i = 10; i <= 400; i++) {
+  console.log(i);
+}
 // Write a loop that will print out every third number starting with 11 and going no higher than 4000.
-//
+for (let i = 11; i <= 4000; i += 3) {
+  console.log(i);
+}
 // 🔴 Commit.
 //
 // B. Get even
