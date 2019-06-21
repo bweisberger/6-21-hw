07f19238a79029f9c6dd7cfc721fc5cda80314b6
William Weisberger <bill.weisberger@gmail.com>
2019-06-21 16:09:16 -0600

    7th commit - Part II, C complete

--- js/app.js
+++ js/app.js
@@ -99,26 +99,26 @@ if (age >= 16){
 //
 // A. The basics
 // Write a loop that will print out all the numbers from 0 to 10, inclusive.
-for (let i = 0; i <= 10; i++) {
-  console.log(i);
-}
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
 // Write a loop that will print out all the numbers from 10 up to and including 400.
-for (let i = 10; i <= 400; i++) {
-  console.log(i);
-}
+// for (let i = 10; i <= 400; i++) {
+//   console.log(i);
+// }
 // Write a loop that will print out every third number starting with 11 and going no higher than 4000.
-for (let i = 11; i <= 4000; i += 3) {
-  console.log(i);
-}
+// for (let i = 11; i <= 4000; i += 3) {
+//   console.log(i);
+// }
 // 🔴 Commit.
 //
 // B. Get even
 // Print out the numbers that are within the range of 1 - 100.
-for (let i = 1; i <= 100; i++) {
-  if (i % 2 === 0) {
-    console.log(i, " <--is an even number");
-  } else { console.log(i); }
-}
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i, " <--is an even number");
+//   } else { console.log(i); }
+// }
 // Adjust your code to add a message next to even numbers only that says: "<-- is an even number".
 //
 // 🔴 Commit.
@@ -126,7 +126,15 @@ for (let i = 1; i <= 100; i++) {
 // C. Give me Five
 // For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five.
 // Example Output:
-//
+for (let i = 0; i <= 100; i++) {
+  if ( i % 3 === 0 && i % 5 === 0) {
+    console.log(`I found a ${i}. Three is a crowd. High five!`)
+  } else if ( i % 3 === 0) {
+    console.log(`I found a ${i}. Three is a crowd.`)
+  } else if ( i % 5 === 0) {
+    console.log(`I found a ${i}. High five!`)
+  }
+}
 // I found a 5. High five!
 // I found a 10. High five!
 // Add to the code from above to print out "I found a number. Three is a crowd" if the number is a multiple of three.
