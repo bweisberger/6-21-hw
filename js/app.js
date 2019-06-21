21cc498446adb92d43f0cf44c5a003b67b9fab55
William Weisberger <bill.weisberger@gmail.com>
2019-06-21 16:19:47 -0600

    10th commit - fixed Part II, D

--- js/app.js
+++ js/app.js
@@ -151,7 +151,14 @@ for (let i = 0; i <= 100; i++) {
 // D. Savings account
 // Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
 // Check your work! Your banck_account should have $55 in it.
-//
+let bank_account = 0;
+for (let i = 1; i <= 100; i++) {
+  // console.log(i);
+  // console.log(bank_account);
+  bank_account += i*2;
+  // console.log(bank_account);
+}
+console.log(bank_account);
 // You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
 // Check your work! Your banck_account should have $10,100 in it.
 //
