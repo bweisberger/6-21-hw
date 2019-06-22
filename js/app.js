f13b0f02cc26c997b9e11a6ccf64780809831722
William Weisberger <bill.weisberger@gmail.com>
2019-06-21 19:13:08 -0600

    16th commit - Part IV, A & B complete.

--- js/app.js
+++ js/app.js
@@ -320,7 +320,7 @@ console.log(thomsCloset);
 //
 // Like so?
 //
-// console.log(printGreeting("Slimer"));
+console.log(printGreeting("Slimer"));
 // => Hello there, Slimer!
 //
 // You think you could? I think so too. Feel free to skip this problem, because you've already done it. If you've done the problem twice, read entire problems carefully before doing them from now on.
@@ -329,8 +329,10 @@ console.log(thomsCloset);
 //
 // B. printCool
 // Write a function printCool that accepts one parameter, name as an argument. The function should print the name and a message saying that that person is cool.
-//
-// console.log(printCool("Captain Reynolds"));
+const printCool = function(name) {
+  console.log(`${name} is cool`);
+}
+console.log(printCool("Captain Reynolds"));
 // => "Captain Reynolds is cool";
 //
 // 🔴 Commit.
